7b4e0e3b641b69d46ea8ddabbc6658cd6b72f187
Kyle Boone <kboone@berkeley.edu>
2015-11-18 17:23:51 -0800

    Added math utilities

--- idrtools/__init__.py
+++ idrtools/__init__.py
@@ -2,3 +2,4 @@ from .tools import IdrToolsException
 from .spectrum import IdrSpectrum, ModifiedSpectrum, Spectrum
 from .supernova import Supernova
 from .dataset import Dataset
+import math
---
+++ idrtools/math.py
@@ -0,0 +1,84 @@
+import numpy as np
+import iminuit
+
+
+def unbiased_std(x):
+    x = np.asarray(x) - np.mean(x)
+    return np.sqrt(np.sum(x*x) / (len(x) - 1))
+
+
+def nmad(x):
+    return 1.4826 * np.median(np.abs(np.asarray(x) - np.median(x)))
+
+
+def rms(x):
+    x = np.asarray(x)
+    return np.sqrt(np.sum(x*x) / len(x))
+
+
+def cum_rms(x):
+    x = np.asarray(x)
+    num = np.cumsum(x**2)
+    denom = np.arange(len(x)) + 1
+    return np.sqrt(num / denom)
+
+
+def cum_nmad(x):
+    out = np.zeros(len(x))
+    for i in range(len(x)):
+        out[i] = nmad(x[:i+1])
+
+    return out
+
+
+def fit_global_values(id_1, id_2, diffs, return_fitted_diffs=False):
+    """Perform a global fit on a set of difference values from a datset."""
+    all_vars = np.unique([id_1, id_2])
+    var_dict = {var: i for i, var in enumerate(all_vars)}
+
+    def global_fit_dist(*vals):
+        # vals are all of the values *except* the first. The first is
+        # set so that the sum of all values is 0. We would have a
+        # reduncancy otherwise.
+        full_vals = [-np.sum(vals)] + list(vals)
+
+        id_1_vals = np.array([full_vals[var_dict[var]] for var in id_1])
+        id_2_vals = np.array([full_vals[var_dict[var]] for var in id_2])
+        global_fit_vals = id_1_vals - id_2_vals
+
+        chisq = ((diffs - global_fit_vals)**2).sum()
+
+        return chisq
+
+    error_guess = np.std(diffs) / np.sqrt(2)
+
+    param_names = ['m' + str(i) for i in range(1, len(all_vars))]
+    params = {i: 0. for i in param_names}
+    params.update({"error_" + i: error_guess for i in param_names})
+    minuit = iminuit.Minuit(
+        global_fit_dist,
+        errordef=1.,
+        forced_parameters=param_names,
+        print_level=0,
+        **params
+    )
+    migrad_result = minuit.migrad()
+
+    if not migrad_result[0].is_valid:
+        print("-"*80)
+        print("ERROR: Global fit failed to converge!")
+        print(migrad_result)
+        print("-"*80)
+        return [0.] * len(all_vars)
+
+    fitted_vals = [minuit.values[minuit.pos2var[i]] for i in
+                   range(len(all_vars)-1)]
+    fitted_vals = [-np.sum(fitted_vals)] + fitted_vals
+
+    if return_fitted_diffs:
+        id_1_vals = np.array([fitted_vals[var_dict[var]] for var in id_1])
+        id_2_vals = np.array([fitted_vals[var_dict[var]] for var in id_2])
+        global_fit_vals = id_1_vals - id_2_vals
+        return (all_vars, fitted_vals, global_fit_vals)
+
+    return all_vars, fitted_vals
