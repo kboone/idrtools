3be1acd3ef24a3a6191507e2d1e1eb85090d967d
Kyle Boone <kboone@berkeley.edu>
2018-04-27 17:35:10 -0700

    Add equal_bin_counts option to plot_binned_function

--- idrtools/math.py
+++ idrtools/math.py
@@ -3,6 +3,10 @@ import iminuit
 from scipy.stats import binned_statistic
 
 
+class IdrToolsMathException(Exception):
+    pass
+
+
 def unbiased_std(x):
     x = np.asarray(x) - np.mean(x)
     return np.sqrt(np.sum(x*x) / (len(x) - 1))
@@ -109,35 +113,120 @@ def plot_windowed_mean(x, y, *args, **kwargs):
     return plot_windowed_function(x, y, np.mean, *args, **kwargs)
 
 
-def plot_binned_function(x, y, func, *args, scatter=False, show_errors=False,
-                         **kwargs):
+def plot_binned_function(x, y, func, bins=10, equal_bin_counts=False,
+                         mode='step', **kwargs):
+    """Plot the given function applied to the y variable, grouped by bins of
+    the x variable.
+
+    - bins and range are passed to scipy.stats.binned_statistic, and can be
+    used in several ways as described in that function's documentation. The
+    simplest usage is for bins to be an integer that specifies how many
+    equal-width bins to have, and for range to be a tuple that specifies the
+    edges of the domain to include.
+    - If equal_bin_counts is True, then bins are created that have the same
+    numbers of counts. Otherwise, bins are created to have the same widths in
+    the x-variable. When equal_bin_counts is True, then bins must be an
+    integer.
+
+    There are several modes available for plotting, specified with the mode
+    keyword.
+    - step: draw a flat line for each group that spans the full width of the
+    group.
+    - scatter: do a scatter plot of the results.
+    - plot: draw lines between the bins, each of which is a single point.
+    - error: plot errorbars for each bin. This is only currently available for
+    a subset of functions where this is somewhat well-defined, and errors are
+    estimated from the data.
+    """
+
     from matplotlib import pyplot as plt
 
     x = np.asarray(x)
     y = np.asarray(y)
 
-    bin_kwargs = {}
-    if 'bins' in kwargs:
-        bin_kwargs['bins'] = kwargs.pop('bins')
-    if 'range' in kwargs:
-        bin_kwargs['range'] = kwargs.pop('range')
+    # range is the keyword used by scipy.stats.binned_function, but it is also
+    # a python keyword that we don't want to overwrite. Ugh. Internally, we use
+    # the term "use_range", but use the variable "range" as the input to this
+    # function for consistency.
+    use_range = kwargs.pop(range, None)
 
-    # Figure out what kind of plot to make
-    mode = kwargs.pop('mode', 'step')
+    bin_kwargs = {
+        'bins': bins,
+        'range': use_range,
+    }
+
+    if equal_bin_counts:
+        # Figure out the percentiles at which we should split the bins.
+        if use_range is not None:
+            use_x = x[(x > use_range[0]) & (x < use_range[1])]
+        else:
+            use_x = x
+
+        use_x = np.sort(use_x)
+
+        bin_edges = [use_x[0]]
+
+        last_idx = 0
+        warned = False
+
+        for i in range(1, bins):
+            next_idx = i * len(use_x) // bins
+
+            # Edge case: if we specify more bins than there are points, bins
+            # will get duplicated. Handle this (somewhat) gracefully by just
+            # not adding more bins.
+            if next_idx == last_idx or next_idx == len(use_x):
+                if not warned:
+                    print("Warning! Specified too many bins in "
+                          "plot_binned_function for equal bin counts. "
+                          "Dropping some bins!")
+                    warned = True
+                continue
+
+            last_idx = next_idx
+
+            next_edge = (use_x[next_idx + 1] + use_x[next_idx]) / 2.
+
+            # Other edge case: if there is a pileup at a certain x-value, then
+            # we can run into issues with putting multiple bin edges at the
+            # same point. Just don't put a bin edge there.
+            if next_edge == bin_edges[-1]:
+                if not warned:
+                    print("Warning! Pileup of x-values at %f in "
+                          "plot_binned_function for equal bin counts. "
+                          "Dropping some bins!" % next_edge)
+                    warned = True
+                continue
+
+            bin_edges.append(next_edge)
+
+        last_bin = use_x[-1]
+        if last_bin == bin_edges[-1]:
+            if not warned:
+                print("Warning! Pileup of x-values at %f in "
+                      "plot_binned_function for equal bin counts. "
+                      "Dropping some bins!" % next_edge)
+                warned = True
+        else:
+            bin_edges.append(last_bin)
+
+        bin_kwargs['bins'] = bin_edges
 
     statistic, bin_edges, binnumber = binned_statistic(x, y, func,
                                                        **bin_kwargs)
 
     bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2.
 
-    if show_errors:
+    if mode == 'error':
         # For median use NMAD for errors, for mean use std.
         if func == 'median':
             err_func = nmad
         elif func == 'mean':
             err_func = np.std
         else:
-            raise Exception("Can't do errors for given function! (%s)" % func)
+            raise IdrToolsMathException(
+                "Can't do errors for given function! (%s)" % func
+            )
 
         err_statistic, err_bin_edges, err_binnumber = binned_statistic(
             x, y, err_func, **bin_kwargs
@@ -156,15 +245,17 @@ def plot_binned_function(x, y, func, *args, scatter=False, show_errors=False,
         errors[mask] = np.nan
         bin_half_widths = (bin_edges[:-1] - bin_edges[1:]) / 2.
         plt.errorbar(bin_centers, statistic, yerr=errors, xerr=bin_half_widths,
-                     *args, **kwargs)
-    elif scatter:
-        plt.scatter(bin_centers, statistic, *args, **kwargs)
+                     **kwargs)
+    elif mode == 'scatter':
+        plt.scatter(bin_centers, statistic, **kwargs)
     elif mode == 'plot':
-        plt.plot(bin_centers, statistic, *args, **kwargs)
+        plt.plot(bin_centers, statistic, **kwargs)
     elif mode == 'step':
         # Have to do a little hacking here since this isn't really built in.
         plt.step(bin_edges, np.hstack([statistic, statistic[-1]]),
-                 where='post', *args, **kwargs)
+                 where='post', **kwargs)
+    else:
+        raise IdrToolsMathException("Unknown mode %s!" % mode)
 
 
 def plot_binned_nmad(x, y, *args, **kwargs):
